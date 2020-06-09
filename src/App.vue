0a456b935711a0f5b93390a179ccb8d9a05b7233
jordihales <46314874+jordihales@users.noreply.github.com>
2020-06-09 17:50:10 +0100

    add axios and blogs

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,57 @@
 <template>
-  <div class="app" :class="(mode === 'dark') ? 'dark' : 'light'">
-    <Header :mode="mode" @toggle="toggle"/>
-    <HelloWorld/>
-    <Banner/>
-    <Skills/>
-    <Social/>
-    <Footer/>
+  <div
+    class="app"
+    :class="mode === 'dark' ? 'dark' : 'light'"
+  >
+    <Header
+      :mode="mode"
+      @toggle="toggle"
+    />
+    <HelloWorld />
+    <Banner />
+    <Skills />
+    <Blog />
+    <Social />
+    <Footer />
   </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
-import HelloWorld from './components/HelloWorld.vue'
-import Banner from './components/Banner.vue'
-import Skills from './components/Skills.vue'
-import Social from './components/Social.vue'
-import Footer from './components/Footer.vue'
+import axios from "axios";
+import Header from "./components/Header.vue";
+import Banner from "./components/Banner.vue";
+import Skills from "./components/Skills.vue";
+import Blog from "./components/Blog.vue";
+import Social from "./components/Social.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
-  name: 'app',
-  data () {
+  name: "app",
+  data() {
     return {
       mode: "light"
-    }
+    };
   },
   components: {
     Header,
-    HelloWorld,
     Banner,
     Skills,
+    Blog,
     Social,
     Footer
   },
   methods: {
-    toggle () {
+    toggle() {
       if (this.mode === "dark") {
-        this.mode = "light"
+        this.mode = "light";
       } else {
-        this.mode = "dark"
+        this.mode = "dark";
       }
     }
   }
-}
+};
 </script>
 
 <style lang="scss">
-  @import "styles/index.scss";
+@import "styles/index.scss";
 </style>
